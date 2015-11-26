57ac1307b2c08802ecdfebb8f2fef9e5f1463682
Ma11k <mikoza@gmail.com>
2015-11-26 22:58:04 +0100

    Все вернул к исходному
    # Conflicts:
    #	js/check.js

--- js/form.js
+++ js/form.js
@@ -18,20 +18,22 @@
 
 
 //Форма и поля ввода
-var formElement = document.forms['review-form'];
-var formMark = formElement['review-form-group-mark"'];
-var formName = formElement['review-name'];
-var formText = formElement['review-text'];
-var formSubmit = formElement['review-submit'];
-var formHint = formElement['review-fields'];
+// var formElement = document.forms['review-form'];
+// var formMark = formElement['review-form-group-mark'];
+// var formName = formElement['review-name'];
+// var formText = formElement['review-text'];
+// var formSubmit = formElement['review-submit'];
+// var formHint = formElement['review-fields'];
+
+var formElement = document.querySelectorAll('form.review-form');
 
 function validation() {
 
-formName.onchange = function(){
-  alert('1111');
-}
-formText.onchange = function(){
-  alert('4544');
+  formName.onchange = function(){
+    alert('1111');
+  }
+  formText.onchange = function(){
+    alert('4544');
 }
 
 }
