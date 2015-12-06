1f941194b532c07793440d6b52202b8b560a59f3
Ma11k <mikoza@gmail.com>
2015-12-06 08:55:40 +0100

    Остались звезды

--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /* global reviews:true */
 
 /**
@@ -8,43 +7,37 @@
  */
 function getElementFromTemplate(data) {
   var template = document.querySelector('#review-template');
-
+  var element;
   if ('content' in template) {
-    var element = template.content.children[0].cloneNode(true);
+    element = template.content.children[0].cloneNode(true);
   } else {
-    var element = template.children[0].cloneNode(true);
+    element = template.children[0].cloneNode(true);
   }
 
   element.querySelector('.review-text').textContent = data.description;
-  console.log('element = ', element);
 
   var backgroundImage = new Image();
   backgroundImage.onload = function() {
     backgroundImage.style.backgroundImage = 'url(\'' + backgroundImage.src + '\')';
-  }
+  };
   backgroundImage.onerror = function() {
     element.classList.add('review-load-failure');
-  }
+  };
   backgroundImage.src = '/' + data.author.picture;
   backgroundImage.classList.add('review-author');
   backgroundImage.style.width = '124px';
   backgroundImage.style.height = '124px';
   backgroundImage.title = data.author.name;
   backgroundImage.alt = data.author.name;
-
-  element.replaceChild(backgroundImage, element.querySelector('.review-author'))
-
-
+  element.replaceChild(backgroundImage, element.querySelector('.review-author'));
   return element;
 }
 
-
 (function() {
+  document.querySelector('.reviews-filter').classList.add('invisible');
   var container = document.querySelector('.reviews-list');
-
-  var element = reviews.forEach(function(item, i, reviews) {
+  reviews.forEach(function(item) {
     var element = getElementFromTemplate(item);
     container.appendChild(element);
   });
-
 })();
